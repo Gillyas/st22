9a629d3ce117006a386618777e30d88d95ac1d45
Gillyas <155540907+Gillyas@users.noreply.github.com>
2024-03-24 19:14:10 +0400

    Add files via upload

---
+++ gaga/src/Skeleton.java
@@ -0,0 +1,5 @@
+public class Skeleton extends FantasyCharacter {
+    public Skeleton(String name, int agility, int health, int experience, int gold, int power) {
+        super(name, agility, health, experience, gold, power);
+    }
+}
---
+++ gaga/src/Trader.java
@@ -0,0 +1,47 @@
+public class Trader implements Seller {
+    private String name;
+    private int gold = 30; // Пример начальной суммы денег
+    private int price = 10;// Стоимость зелья
+
+    public Trader(String торговец, int gold, int price) {
+        this.name = торговец;
+        this.gold = gold;
+        this.price = price;
+    }
+
+    @Override
+    public String sell(Goods goods) {
+        String result = "";
+        if (goods == Goods.POTION) {
+            if (hasEnoughMoney(10)) {
+                updateMoney(10);
+
+                if (gold >= 10) {
+                    result = "Вы купили зелье за " + price + " золота. У вас осталось " + gold + " золота." +
+                            " Желаете продолжить покупки? (да/нет)";
+                    increaseGold(10); // Добавляем увеличение денег у торговца после совершения покупки
+                } else {
+                    result = "Слишком мало денег. Хотите выйти? (да/нет)";
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean hasEnoughMoney(int price) {
+        return gold >= price;
+    }
+
+    public void updateMoney(int price) {
+        gold -= price;
+    }
+
+    public void increaseGold(int goldAmount) {
+        gold += goldAmount;
+        System.out.println("Торговец получил " + goldAmount + " золота. Теперь у него " + gold + " золота.");
+    }
+
+    public enum Goods {
+        POTION
+    }
+}
